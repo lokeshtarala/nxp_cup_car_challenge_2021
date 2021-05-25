c50913f287a9b21f9a89321d8fb23b0543df7269
lokeshtarala <lokeshtarala@gmail.com>
2021-05-25 12:20:29 +0530

    made joy control

--- aim_line_follow/aim_line_follow/aim_line_follow.py
+++ aim_line_follow/aim_line_follow/aim_line_follow.py
@@ -8,6 +8,7 @@ from rcl_interfaces.msg import ParameterDescriptor
 
 from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
+from sensor_msgs.msg import Joy
 from std_msgs.msg import Float64
 from nxp_cup_interfaces.msg import PixyVector
 from time import sleep
@@ -19,59 +20,17 @@ class LineFollow(Node):
     def __init__(self):
         super().__init__('aim_line_follow')
 
+        #variables
+        self.speed_vector = Vector3()
+        self.steer_vector = Vector3()
+        self.cmd_vel_car = Twist()
+        self.axes = [0,0,0,0,0,0,0] 
+        self.speed =0.0
+        self.steer = 0.0
 
-        start_delay_descriptor = ParameterDescriptor(
-            type=ParameterType.PARAMETER_DOUBLE,
-            description='Seconds to delay before starting.')
+       
 
-        camera_vector_topic_descriptor = ParameterDescriptor(
-            type=ParameterType.PARAMETER_STRING,
-            description='Namespaceing with camera topic.')
-        
-        linear_velocity_descriptor = ParameterDescriptor(
-            type=ParameterType.PARAMETER_DOUBLE,
-            description='Linear velocity for vehicle motion (m/s).')
-
-        angular_velocity_descriptor = ParameterDescriptor(
-            type=ParameterType.PARAMETER_DOUBLE,
-            description='Angular velocity for vehicle motion (rad/s).')
-
-        single_line_steer_scale_descriptor = ParameterDescriptor(
-            type=ParameterType.PARAMETER_DOUBLE,
-            description='Single found line steer scaling.')
-        
-        self.declare_parameter("start_delay", 15.0, 
-            start_delay_descriptor)
-        
-        self.declare_parameter("camera_vector_topic", "/cupcar0/PixyVector", 
-            camera_vector_topic_descriptor)
-        
-        self.declare_parameter("linear_velocity", 1.25, 
-            linear_velocity_descriptor)
-
-        self.declare_parameter("angular_velocity", 1.5, 
-            angular_velocity_descriptor)
-
-        self.declare_parameter("single_line_steer_scale", 0.5, 
-            single_line_steer_scale_descriptor)
-
-        self.start_delay = float(self.get_parameter("start_delay").value)
-
-        self.camera_vector_topic = str(self.get_parameter("camera_vector_topic").value)
-
-        self.linear_velocity = float(self.get_parameter("linear_velocity").value)
-
-        self.angular_velocity = float(self.get_parameter("angular_velocity").value)
-
-        self.single_line_steer_scale = float(self.get_parameter("single_line_steer_scale").value)
-
-        # Time to wait before running
-        self.get_logger().info('Waiting to start for {:s}'.format(str(self.start_delay)))
-        sleep(self.start_delay)
-        self.get_logger().info('Started')
-
-        self.start_time = datetime.now().timestamp()
-        self.restart_time = True
+        self.camera_vector_topic = '/cupcar0/PixyVector'
 
         # Subscribers
         self.pixy_subscriber = self.create_subscription(
@@ -79,93 +38,54 @@ class LineFollow(Node):
             self.camera_vector_topic,
             self.listener_callback,
             10)
+        
+
+        # Subscribers
+        self.joy_subscriber = self.create_subscription(
+            Joy,
+            '/joy',
+            self.joy_callback,
+            10)
+
+        
+        
 
         # Publishers
         self.cmd_vel_publisher = self.create_publisher(Twist, '/cupcar0/cmd_vel', 10)
 
-        self.speed_vector = Vector3()
-        self.steer_vector = Vector3()
-        self.cmd_vel = Twist()
+        
+
+
 
         # Timer setup
         # timer_period = 0.5 #seconds
         # self.timer = self.create_timer(timer_period, self.timer_callback)
         # self.i = 0
 
-    def get_num_vectors(self, msg):
-        num_vectors = 0
-        if(not(msg.m0_x0 == 0 and msg.m0_x1 == 0 and msg.m0_y0 == 0 and msg.m0_y1 == 0)):
-            num_vectors = num_vectors + 1
-        if(not(msg.m1_x0 == 0 and msg.m1_x1 == 0 and msg.m1_y0 == 0 and msg.m1_y1 == 0)):
-            num_vectors = num_vectors + 1
-        return num_vectors
+    
 
     # def timer_callback(self):
     #     #TODO
+    def joy_callback(self,msg):
+        self.axes = msg.axes
+        self.speed = self.axes[1]
+        self.speed = (self.speed )*1.5  
+        self.steer = self.axes[3]
+        self.steer = (self.steer + 1.0)*0.65 - 0.65
+        print(self.steer)
+        print(self.speed)
+        
 
     def listener_callback(self, msg):
         #TODO
-        current_time = datetime.now().timestamp()
-        frame_width = 79
-        frame_height = 52
-        window_center = (frame_width / 2)
-        x = 0
-        y = 0
-        steer = 0
-        speed = 0
-        num_vectors = self.get_num_vectors(msg)
+        
 
-        if(num_vectors == 0):
-            if self.restart_time:
-                self.start_time = datetime.now().timestamp()
-                self.restart_time = False
-            if (self.start_time+4.0) > current_time:
-                speed = self.linear_velocity * (4.0-(current_time-self.start_time))/4.0
-            if (self.start_time+4.0) <= current_time:
-                speed = 0
-            steer = 0
+        self.speed_vector.x = self.speed
+        self.steer_vector.z = self.steer
+        self.cmd_vel_car.linear = self.speed_vector
+        self.cmd_vel_car.angular = self.steer_vector
 
-        if(num_vectors == 1):
-            if not self.restart_time:
-                self.start_time = datetime.now().timestamp()
-                self.restart_time = True
-            if(msg.m0_x1 > msg.m0_x0):
-                x = (msg.m0_x1 - msg.m0_x0) / frame_width
-                y = (msg.m0_y1 - msg.m0_y0) / frame_height
-            else:
-                x = (msg.m0_x0 - msg.m0_x1) / frame_width
-                y = (msg.m0_y0 - msg.m0_y1) / frame_height
-            if(msg.m0_x0 != msg.m0_x1 and y != 0):
-                steer = (-self.angular_velocity) * (x / y) * self.single_line_steer_scale
-                if (self.start_time+4.0) > current_time:
-                    speed = self.linear_velocity * ((current_time-self.start_time)/4.0)
-                if (self.start_time+4.0) <= current_time:
-                    speed = self.linear_velocity
-            else:
-                steer = 0
-                if (self.start_time+4.0) > current_time:
-                    speed = self.linear_velocity * ((current_time-self.start_time)/4.0)*0.9
-                if (self.start_time+4.0) <= current_time:
-                    speed = self.linear_velocity*0.9
-
-        if(num_vectors == 2):
-            if not self.restart_time:
-                self.start_time = datetime.now().timestamp()
-                self.restart_time = True
-            m_x1 = (msg.m0_x1 + msg.m1_x1) / 2
-            steer = self.angular_velocity*(m_x1 - window_center) / frame_width
-            if (self.start_time+4.0) > current_time:
-                speed = self.linear_velocity * ((current_time-self.start_time)/4.0)
-            if (self.start_time+4.0) <= current_time:
-                speed = self.linear_velocity
-
-        self.speed_vector.x = float(speed*(1-np.abs(2.0*steer)))
-        self.steer_vector.z = float(steer)
-
-        self.cmd_vel.linear = self.speed_vector
-        self.cmd_vel.angular = self.steer_vector
-
-        self.cmd_vel_publisher.publish(self.cmd_vel)
+        self.cmd_vel_publisher.publish(self.cmd_vel_car)
 
 def main(args=None):
     rclpy.init(args=args)
